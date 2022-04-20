b3f71d64bfb51b11fd2a528e28bc732fe7d13bd2
penguin0001 <robynpgreer@gmail.com>
2022-04-20 19:49:02 +0100

    updated results messages

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 function computerPlay() {
     // randomly returns either rock, paper or scissors
     const n = Math.floor(Math.random() * 3);
-    const options = ["Rock", "Paper", "Scissors"];
+    const options = ["rock", "paper", "scissors"];
     return options[n];
 }
 
@@ -13,15 +13,15 @@ function playRound(playerSelection, computerSelection) {
     let result = '';
 
     if (ps == cs) {
-        result = "Draw!";
+        result = "draw!";
     } else if ((ps == "rock" && cs == "scissors") ||
                 (ps == "scissors" && cs == "paper") ||
                 (ps == "paper" && cs == "rock")) {
-        result = "You win!"; 
+        result = "you win! " + ps + " beats " + cs + "!"; 
     } else if ((cs == "rock" && ps == "scissors") ||
                 (cs == "scissors" && ps == "paper") ||
                 (cs == "paper" && ps == "rock")) {
-        result = "You lose!";
+        result = "you lose! " + cs + " beats " + ps + "!"; ;
     } else {
         result = "wtf";
     }
@@ -30,5 +30,5 @@ function playRound(playerSelection, computerSelection) {
 }
 
 let comp = computerPlay();
-console.log(comp);
+console.log("computer chooses: " + comp);
 console.log(playRound("rock", comp));
