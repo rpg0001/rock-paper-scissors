f378d74a9843cad9eb9b76f16a0d2b092feac928
penguin0001 <robynpgreer@gmail.com>
2022-04-20 19:45:53 +0100

    added main game logic

--- script.js
+++ script.js
@@ -6,4 +6,29 @@ function computerPlay() {
     return options[n];
 }
 
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection) {
+    const ps = playerSelection.toLowerCase();
+    const cs = computerSelection.toLowerCase();
+
+    let result = '';
+
+    if (ps == cs) {
+        result = "Draw!";
+    } else if ((ps == "rock" && cs == "scissors") ||
+                (ps == "scissors" && cs == "paper") ||
+                (ps == "paper" && cs == "rock")) {
+        result = "You win!"; 
+    } else if ((cs == "rock" && ps == "scissors") ||
+                (cs == "scissors" && ps == "paper") ||
+                (cs == "paper" && ps == "rock")) {
+        result = "You lose!";
+    } else {
+        result = "wtf";
+    }
+
+    return result;
+}
+
+let comp = computerPlay();
+console.log(comp);
+console.log(playRound("rock", comp));
